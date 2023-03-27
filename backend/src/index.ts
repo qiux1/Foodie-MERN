d3f8f9a89f7280526d110d3aa0383ba62ee6c3f6
Xinyu Qiu <qiux1@seattleu.edu>
2023-03-26 19:40:23 -0700

    set up the basic app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello World");
+import * as dotenv from 'dotenv'
+dotenv.config();
+import express, {Request, Response, Application} from 'express'
+import cors from 'cors'
+import helmet from 'helmet';
+
+
+const app: Application = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}));
+app.use(cors());
+app.use(helmet());
+
+app.all('*', (req: Request, res: Response)=>{
+    res.status(404).json({message: "The route you requested is not found"})
+});
+
+const PORT = (process.env.PORT as unknown as number) || 5000
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server listening on port ${PORT}`);
+})
